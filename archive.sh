7e9729b1c75ae629d2a726ff22b3e6185706e903
Syadonn <tommounet@gmail.com>
2018-01-18 17:00:32 +0100

    Update archive.sh
    This first version don't care about what's inside a folder if he found one.
    It's not recursive

--- archive.sh
+++ archive.sh
@@ -1,27 +1,25 @@
-cd ~/Téléchargements					#Change this line to change this concerned directory
-
 timemonth=$(date -d "-1 month" '+%s') 	#We get the timestamp of a month ago
 namedir=$(date -d "-1 month" '+%B_%Y') 	#We get the name of the future zipped dir (monthname_year)
 
-nb=$(ls| wc -l) 						#We count the number of files in the working directory
+nb=$(ls| wc -l) #We count the number of files in the working directory
 
-ls > temp 								#We get all the files names and store them in temp
+ls > temp #We get all the files names and store them in temp
 mkdir $namedir
-touch liste_fichiers.txt
+touch files_list.txt
 
-while [[ $nb -ge 1 ]] 					#While we haven't scan every file/dir
+while [[ $nb -ge 1 ]] #While we haven't scan every file/dir
 do
 	namefile=$(cat temp | head -n $nb| tail -n 1) 	#We get the file's name
-	timefile=$(date -r "$namefile" '+%s') 			#We get the file's timestamp
+	timefile=$(date -r "$namefile" '+%s') 		#We get the file's timestamp
 
 	if [[ $timefile -lt $timemonth ]]; then
 		mv "$namefile" $namedir
-		echo "$namefile" >> liste_fichiers.txt
+		echo "$namefile" >> files_list.txt
 	fi
 
 	nb=$(($nb-1))
 done
 
-mv liste_fichiers.txt $namedir
+mv files_list.txt $namedir
 zip -r $namedir.zip $namedir
 rm -rf temp $namedir
